4633d0eb0cfc0b2897f78cb09a8f6c02ab153f62
Damir Kusar <damir@kusar.ch>
2015-07-03 18:11:31 +0200

    commented out some warnings

--- generators/app/templates/public/app.js
+++ generators/app/templates/public/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-require('./modules/core');
+require('./modules/core'); // jshint ignore:line
--- generators/module/templates/module/index.js
+++ generators/module/templates/module/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-ApplicationConfiguration.registerModule('<%= moduleName %>');
+ApplicationConfiguration.registerModule('<%= moduleName %>'); // jshint ignore:line
 
-require('./js/config/<%= moduleName %>.route.js');
-require('./js/config/<%= moduleName %>.menu.js');
-require('./js/controllers/<%= moduleName %>.controller.js');
-require('./js/directives/<%= moduleName %>.directive.js');
-require('./js/services/<%= moduleName %>.service.js');
+require('./js/config/<%= moduleName %>.route.js'); // jshint ignore:line
+require('./js/config/<%= moduleName %>.menu.js'); // jshint ignore:line
+require('./js/controllers/<%= moduleName %>.controller.js'); // jshint ignore:line
+require('./js/directives/<%= moduleName %>.directive.js'); // jshint ignore:line
+require('./js/services/<%= moduleName %>.service.js'); // jshint ignore:line
